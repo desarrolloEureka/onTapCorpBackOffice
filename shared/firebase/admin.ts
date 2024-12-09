6112feded636ff853c2b6d7017880ebfe8228153
juansebas2000 <juan@eurekadreams.com>
2024-12-09 13:04:03 -0500

    prueba-variables-entorno-vercel

--- shared/firebase/admin.ts
+++ shared/firebase/admin.ts
@@ -3,22 +3,22 @@ export default function initFirebase() {
   if (!admin.apps.length) {
     admin.initializeApp({
       credential: admin.credential.cert({
-        type: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_SDK_TYPE,
-        project_id: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_SDK_PROJECT_ID,
+        type: process.env.NEXT_PRIVATE_FIREBASE_ADMIN_SDK_TYPE,
+        project_id: process.env.NEXT_PRIVATE_FIREBASE_ADMIN_SDK_PROJECT_ID,
         private_key_id:
-          process.env.NEXT_PUBLIC_FIREBASE_ADMIN_SDK_PRIVATE_KEY_ID,
-        private_key: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_SDK_PRIVATE_KEY,
-        client_email: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_SDK_CLIENT_EMAIL,
-        client_id: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_SDK_CLIENT_ID,
-        auth_uri: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_SDK_AUTH_URI,
-        token_uri: process.env.NEXT_PUBLIC_FIREBASE_ADMIN_SDK_TOKEN_URI,
+          process.env.NEXT_PRIVATE_FIREBASE_ADMIN_SDK_PRIVATE_KEY_ID,
+        private_key: process.env.NEXT_PRIVATE_FIREBASE_ADMIN_SDK_PRIVATE_KEY,
+        client_email: process.env.NEXT_PRIVATE_FIREBASE_ADMIN_SDK_CLIENT_EMAIL,
+        client_id: process.env.NEXT_PRIVATE_FIREBASE_ADMIN_SDK_CLIENT_ID,
+        auth_uri: process.env.NEXT_PRIVATE_FIREBASE_ADMIN_SDK_AUTH_URI,
+        token_uri: process.env.NEXT_PRIVATE_FIREBASE_ADMIN_SDK_TOKEN_URI,
         auth_provider_x509_cert_url:
           process.env
-            .NEXT_PUBLIC_FIREBASE_ADMIN_SDK_AUTH_PROVIDER_X509_CERT_URL,
+            .NEXT_PRIVATE_FIREBASE_ADMIN_SDK_AUTH_PROVIDER_X509_CERT_URL,
         client_x509_cert_url:
-          process.env.NEXT_PUBLIC_FIREBASE_ADMIN_SDK_CLIENT_X509_CERT_URL,
+          process.env.NEXT_PRIVATE_FIREBASE_ADMIN_SDK_CLIENT_X509_CERT_URL,
         universe_domain:
-          process.env.NEXT_PUBLIC_FIREBASE_ADMIN_SDK_UNIVERSE_DOMAIN,
+          process.env.NEXT_PRIVATE_FIREBASE_ADMIN_SDK_UNIVERSE_DOMAIN,
       } as ServiceAccount),
     });
   }
--- shared/firebase/firebase.tsx
+++ shared/firebase/firebase.tsx
@@ -22,28 +22,3 @@ const db = firebaseApp.firestore();
 const auth = firebase.auth();
 
 export { db, auth, firebaseConfig };
-
-console.log(
-    process.env.NEXT_PUBLIC_FIREBASE_ADMIN_SDK_TYPE,
-    process.env.NEXT_PUBLIC_FIREBASE_ADMIN_SDK_PROJECT_ID,
-    process.env.NEXT_PUBLIC_FIREBASE_ADMIN_SDK_PRIVATE_KEY_ID,
-    process.env.NEXT_PUBLIC_FIREBASE_ADMIN_SDK_PRIVATE_KEY,
-    process.env.NEXT_PUBLIC_FIREBASE_ADMIN_SDK_CLIENT_EMAIL,
-    process.env.NEXT_PUBLIC_FIREBASE_ADMIN_SDK_CLIENT_ID,
-    process.env.NEXT_PUBLIC_FIREBASE_ADMIN_SDK_AUTH_URI,
-    process.env.NEXT_PUBLIC_FIREBASE_ADMIN_SDK_TOKEN_URI,
-    process.env.NEXT_PUBLIC_FIREBASE_ADMIN_SDK_AUTH_PROVIDER_X509_CERT_URL,
-    process.env.NEXT_PUBLIC_FIREBASE_ADMIN_SDK_CLIENT_X509_CERT_URL,
-    process.env.NEXT_PUBLIC_FIREBASE_ADMIN_SDK_UNIVERSE_DOMAIN
-  );
-
-  console.log(
-    process.env.NEXT_PUBLIC_API_KEY,
-     process.env.NEXT_PUBLIC_AUTH_DOMAIN,
-     process.env.NEXT_PUBLIC_PROJECT_ID,
-     process.env.NEXT_PUBLIC_STORAGE_BUCKET,
-     process.env.NEXT_PUBLIC_MESSAGING_SENDER_ID,
-     process.env.NEXT_PUBLIC_APP_ID,
-     process.env.NEXT_PUBLIC_MEASUREMENT_ID,
-     process.env.NEXT_PUBLIC_BACKEND_BASE_URL,
-  );
