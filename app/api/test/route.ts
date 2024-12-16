c6ae8f2852aedc0f75f2d98d613d9d5069dddb73
juansebas2000 <juan@eurekadreams.com>
2024-12-16 12:58:52 -0500

    first-test-consolelog-newDate

---
+++ app/api/test/route.ts
@@ -0,0 +1,7 @@
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(request: NextRequest) {
+  console.log("Cron Job Ran at: ", new Date());
+
+  return NextResponse.json({ message: "Cron Job Ran at " + new Date() });
+}
