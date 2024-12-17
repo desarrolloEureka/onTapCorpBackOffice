abf4243bbc354c3c85a1106b0fdd9c9b925018a6
juansebas2000 <juan@eurekadreams.com>
2024-12-17 10:19:43 -0500

    fourth-test-consolelog-newDate

--- app/api/test/route.ts
+++ app/api/test/route.ts
@@ -1,14 +1,26 @@
 import { NextRequest, NextResponse } from "next/server";
+export const dynamic = 'force-dynamic'; // static by default, unless reading the request
 
-export async function GET(request: NextRequest) {
-  const authHeader = request.headers.get("authorization");
-  if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
-    return new Response("Unauthorized", {
-      status: 401,
-    });
-  }
+// export async function GET(request: NextRequest) {
+//   const authHeader = request.headers.get("authorization");
+//   if (authHeader !== `Bearer ${process.env.CRON_SECRET}`) {
+//     return new Response("Unauthorized", {
+//       status: 401,
+//     });
+//   }
 
-  console.log("Cron Job Ran at: ", new Date());
+//   console.log("Cron Job Ran at: ", new Date());
 
-  return NextResponse.json({ message: "Cron Job Ran at " + new Date() });
+//   return NextResponse.json({ message: "Cron Job Ran at " + new Date() });
+// }
+
+
+// export async function GET(request: NextRequest) {
+//   console.log("Cron Job Ran at: ", new Date());
+//   return NextResponse.json({ message: "Cron Job Ran at " + new Date() });
+// }
+
+ 
+export function GET(request: Request) {
+  return new Response(`Hello from ${process.env.CRON_SECRET}`);
 }
